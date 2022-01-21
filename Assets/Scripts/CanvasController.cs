82d0a9bb4fad92144ec26288cbeeaa344889f838
Ryan Ricardo Rossi <70291872+ryanricardo@users.noreply.github.com>
2022-01-21 04:03:51 -0300

    controller waves, gameover canvas

--- Assets/Scripts/CanvasController.cs
+++ Assets/Scripts/CanvasController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
@@ -9,6 +10,7 @@ public class CanvasController : MonoBehaviour
 
     [Header("Components")]
     [SerializeField]        private         TextMeshProUGUI         textWave;
+    [SerializeField]        private         GameObject              panelGameOver;
     [HideInInspector]       private         Manager                 mg;
 
     void Start()
@@ -21,8 +23,23 @@ public class CanvasController : MonoBehaviour
         
     }
 
+    public void ButtonPlayAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void RefreshWave()
     {
         textWave.text = "Wave: " + mg.waveCurrent;
+    }
+
+    public void PanelGameOver()
+    {
+        panelGameOver.SetActive(true);
     }
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -2,25 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Manager : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField]        private         Transform[]         spawnBots;
-    [SerializeField]        private         GameObject          bot;
+    [SerializeField]        private         GameObject[]        bot;
     [HideInInspector]       private         CanvasController    canvasController;
+    [HideInInspector]       private         PlayerController    playerController;
 
     [Header("Atributtes scene")]
     [SerializeField]        public          int                 waveCurrent;
     [SerializeField]        public          int                 countBots;
     void Start()
     {
+        playerController = FindObjectOfType<PlayerController>();
         canvasController = FindObjectOfType<CanvasController>();
         waveCurrent = 1;
+        Time.timeScale = 1;
     }
 
     void Update()
     {
         ControllerBots();
+        ControllerScene();
     }
 
     void ControllerBots()
@@ -29,11 +34,33 @@ public class Manager : MonoBehaviour
         {
             do
             {
-                Instantiate(bot, spawnBots[Random.Range(0, 18)].transform.position, Quaternion.identity);
+
+                if(waveCurrent > 10)
+                {
+                    Instantiate(bot[Random.Range(0, 5)], spawnBots[Random.Range(0, 18)].transform.position, Quaternion.identity);
+                }else 
+                {
+                    Instantiate(bot[0], spawnBots[Random.Range(0, 18)].transform.position, Quaternion.identity);
+                }
                 countBots += 1;
                 canvasController.RefreshWave();
             }while(countBots < waveCurrent);
-            waveCurrent += 1;
+
+            if(waveCurrent < 15)
+            {
+                waveCurrent += 1;
+            }
+        }
+    }
+
+
+
+    public void ControllerScene()
+    {
+        if(playerController.death)
+        {
+            Time.timeScale = 0;
+            canvasController.PanelGameOver();
         }
     }
 
